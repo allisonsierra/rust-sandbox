aac5e459b0aa64b30a7e5169eb42840fa97b7bab
Allie Sierra <85902135+allisonsierra@users.noreply.github.com>
2022-02-05 16:20:15 -0500

    quick_xml

--- rocket/src/securityroles.rs
+++ rocket/src/securityroles.rs
@@ -1,15 +1,16 @@
 use quick_xml::de::{from_str, DeError};
-use serde::Deserialize;
+use quick_xml::se::to_string;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 #[serde(rename = "security")]
 pub struct SecurityRoles {
     #[serde(rename = "userRoleMappings")]
     user_role_mappings: Vec<UserRoleMapping>,
 }
 
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-#[serde(rename_all(deserialize = "camelCase"))]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct UserRoleMapping {
     user_id: String,
     source: String,
@@ -21,8 +22,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_should_deserialize_from_xml() {
-        let xml_simple = r#"<?xml version="1.0" encoding="UTF-8"?>
+    fn it_should_serialize_to_xml() {
+        let roles = SecurityRoles {
+            user_role_mappings: vec![UserRoleMapping {
+                user_id: "user-with-one-role".to_string(),
+                source: "default".to_string(),
+                roles: vec!["org.group.4users-deployer".to_string()],
+            }],
+        };
+
+        let expected = r#"<?xml version="1.0" encoding="UTF-8"?>
         <security>
           <userRoleMappings>
             <userRoleMapping>
@@ -36,27 +45,48 @@ mod tests {
         </security>
         "#;
 
+        let actual = to_string(&roles).expect("Failed to serialize XML");
+        dbg!(&actual);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn it_should_deserialize_from_xml() {
+        let xml_simple = r#"<?xml version="1.0" encoding="UTF-8"?>
+      <security>
+        <userRoleMappings>
+          <userRoleMapping>
+            <userId>user-with-one-role</userId>
+            <source>default</source>
+            <roles>
+              <role>org.group.4users-deployer</role>
+            </roles>
+          </userRoleMapping>
+        </userRoleMappings>
+      </security>
+      "#;
+
         let xml_complex = r#"<?xml version="1.0" encoding="UTF-8"?>
-        <security>
-          <userRoleMappings>
-            <userRoleMapping>
-              <userId>user-with-one-role</userId>
-              <source>default</source>
-              <roles>
-                <role>org.group.4users-deployer</role>
-              </roles>
-            </userRoleMapping>
-            <userRoleMapping>
-              <userId>user-with-two-roles</userId>
-              <source>default</source>
-              <roles>
-                <role>org.group.4users-deployer</role>
-                <role>org.group.3users-deployer</role>
-              </roles>
-            </userRoleMapping>
-          </userRoleMappings>
-        </security>
-        "#;
+      <security>
+        <userRoleMappings>
+          <userRoleMapping>
+            <userId>user-with-one-role</userId>
+            <source>default</source>
+            <roles>
+              <role>org.group.4users-deployer</role>
+            </roles>
+          </userRoleMapping>
+          <userRoleMapping>
+            <userId>user-with-two-roles</userId>
+            <source>default</source>
+            <roles>
+              <role>org.group.4users-deployer</role>
+              <role>org.group.3users-deployer</role>
+            </roles>
+          </userRoleMapping>
+        </userRoleMappings>
+      </security>
+      "#;
 
         let expected: SecurityRoles = SecurityRoles {
             user_role_mappings: vec![UserRoleMapping {
